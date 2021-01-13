3f1c34e741526fb6b0b99bfd2afd683565269b5e
akshay_2304 <toakshay.official@gmail.com>
2021-01-13 17:14:13 +0530

    List view for each playlist

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,17 +4,17 @@
       <p>{{ error }}</p>
     </div>
     <div v-if="documents">
-      <div v-for="doc in documents" :key="doc.id">
-        {{doc.title}}
-      </div>
+      <ListView :playlists="documents"/>
     </div>
   </div>
 </template>
 
 <script>
 import getCollection from '../composables/getCollection'
+import ListView from '../components/ListView.vue'
 export default {
   name: 'Home',
+  components : { ListView },
   setup(){
     const {error, documents} = getCollection('playlists')
 

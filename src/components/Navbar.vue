97b97b3efd36fd8387f93624e7e7ddccfdb7e5c6
akshay_2304 <toakshay.official@gmail.com>
2021-01-13 15:08:06 +0530

    Navbar setup

---
+++ src/components/Navbar.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="navbar">
+      <nav>
+          <h1><router-link :to="{ name : 'Home' }">D3 Playlist</router-link></h1>
+          <div class="links">
+              <button>Log Out</button>
+              <router-link class="btn" :to="{ name : 'Signup' }">Sign Up</router-link>
+              <router-link class="btn" :to="{ name : 'Login' }">Log In</router-link>
+          </div>
+      </nav>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+    .navbar{
+        padding: 16px 10px;
+        margin-bottom: 60px;
+        background: white;
+    }
+    nav{
+        display: flex;
+        max-width: 1200px;
+        align-items: center;
+        justify-content: space-between;
+        margin: 0 auto;
+    }
+    nav h1{
+        margin-left: 20px;
+    }
+    nav .links{
+        margin-left: auto;
+    }
+    nav .links a, button{
+        margin-left: 16px;
+        font-size: 14px;
+    }
+</style>

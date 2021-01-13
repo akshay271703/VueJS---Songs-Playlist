df82fefcbe4e12ff94498b7dd7a069f864628c68
akshay_2304 <toakshay.official@gmail.com>
2021-01-13 16:56:34 +0530

    Realtime data listener on Homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,24 @@
 <template>
   <div class="home">
-    
+    <div class="error" v-if="error">
+      <p>{{ error }}</p>
+    </div>
+    <div v-if="documents">
+      <div v-for="doc in documents" :key="doc.id">
+        {{doc.title}}
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-
+import getCollection from '../composables/getCollection'
 export default {
   name: 'Home',
-  components: { }
+  setup(){
+    const {error, documents} = getCollection('playlists')
+
+    return { error , documents}
+  }
 }
 </script>
